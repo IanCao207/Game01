1fe11283238a8a83541c93822f76b43dc5a66af1
IanCao207 <ian950407@gmail.com>
2025-12-23 14:11:16 +0800

    01

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public int HP;
+
+    //初始化
+    void Start()
+    {
+        HP = 100;
+    }
+
+    //更新:偵測操作
+    void Update()
+    {   
+        
+    }
+}
