22df7b54185f49fabaa455a8a94964b99ad8748a
IanCao207 <ian950407@gmail.com>
2025-12-24 16:33:27 +0800

    1224

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -9,25 +9,59 @@ public class PlayerCtrl : MonoBehaviour
 {
     #region 基本參數
     public CharacterController charCtrl;
+    /// <summary>
+    /// 角色血量
+    /// </summary>
     public int HP;
+    /// <summary>
+    /// 外部輸入
+    /// </summary>
+    Vector2 input;
+    Vector3 look;
     #endregion 基本參數
 
+    #region 角色公開狀態
+    /// <summary>
+    /// 角色是否有接收輸入操作
+    /// </summary>
+    public bool isMove => input != Vector2.zero;
+    #endregion 角色公開狀態
+
     #region 生命週期
-    //初始化
+    /// <summary>
+    /// 初始化
+    /// </summary>
     void Start()
     {
         HP = 100;
     }
 
-    //更新:偵測操作
+    /// <summary>
+    /// 更新:偵測操作
+    /// </summary>
     void Update()
     {
+        Action();
+    }
+    #endregion 生命週期
 
+    #region 操作設計
+    /// <summary>
+    /// 角色行動
+    /// </summary>
+    void Action()
+    {
+        //轉動角色
+        look.z = input.y;
+        look.x = input.x;
+        transform.rotation = Quaternion.LookRotation(look);
+        //角色控制器.移動(往前)
+        if (isMove) charCtrl.SimpleMove(transform.forward);
     }
 
     public void Move(CallbackContext callback)
     {
-        //角色控制器.移動(往前)
-        charCtrl.SimpleMove(Vector3.forward);
+        input = callback.ReadValue<Vector2>();
     }
+    #endregion 操作設計
 }
